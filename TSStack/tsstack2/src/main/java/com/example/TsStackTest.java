c92b21666623a33d0d762ef888611c5c05c396c8
Lance Lebanoff <lancelebanoff@yahoo.com>
2016-02-06 16:21:12 -0500

    Added a little extra to the test

--- TSStack/tsstack2/src/main/java/com/example/TsStackTest.java
+++ TSStack/tsstack2/src/main/java/com/example/TsStackTest.java
@@ -19,6 +19,10 @@ public class TsStackTest {
         spBuffer.insSp(18);
         nodePair = spBuffer.getSp();
         spBuffer.tryRemSP(nodePair.oldTop, nodePair.result);
+        for(int i = 0; i < 10; i++) {
+            nodePair = spBuffer.getSp();
+            spBuffer.tryRemSP(nodePair.oldTop, nodePair.result);
+        }
 
         System.out.println("Printing SpBuffer");
 
